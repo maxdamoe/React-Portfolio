60ac77484470c90a964db54c9d760ac3ce4f0d61
msimp <max.simpson@gmail.com>
2021-10-10 22:40:54 -0500

    switching to desktop

---
+++ react-portfolio/src/helpers/navigation.js
@@ -0,0 +1,22 @@
+export const navLinks = [
+  {
+    name: "HOME",
+    ref: "#home",
+  },
+  {
+    name: "TECH",
+    ref: "#tech",
+  },
+  {
+    name: "PORTFOLIO",
+    ref: "#portfolio",
+  },
+  {
+    name: "ABOUT ME",
+    ref: "#about-me",
+  },
+  {
+    name: "CONTACT",
+    ref: "#contact",
+  },
+];
---
+++ react-portfolio/src/helpers/techs.js
@@ -0,0 +1,30 @@
+export const techs = [
+    {
+      name: "React.js",
+    },
+    {
+      name: "Node.js",
+    },
+    {
+      name: "Javascript",
+    },
+    {
+      name: "CSS",
+    },
+    {
+      name: "HTML",
+    },
+    {
+      name: "Mongo DB",
+    },
+    {
+      name: "Bootstrap",
+    },
+    {
+      name: "SQL",
+    },
+    {
+      name: "Github",
+    },
+  ];
+  
